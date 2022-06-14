755b4a3c4a7d1abbef76d0f38b398b7e0a23778d
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-14 23:47:06 +0300

    commit tree 400d5de50e5c5429cfe6b9f54cf4e3453ab5cd4d

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
 }
+let input;
+input = document.getElementById("input")
+function addingEventListener() {
+    input.addEventListener('click', event => input.innerText = 'Hi')
+}
+
+addingEventListener() 
